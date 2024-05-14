2229a8612b517fcb32504097b8ce201808516267
Emanuel Ruiz <emanuel.u.ruiz@gmail.com>
2024-05-13 18:20:24 -0700

    add get_item and keys methods

--- hash_tables.py
+++ hash_tables.py
@@ -22,10 +22,29 @@ class Hash_Table:
         self.data_map[index].append([key, value]);
         return True;
 
+    def get_item(self, key):
+        index = self.__hash(key);
+        if self.data_map[index] == None:
+            print(key);
+            return None;
+        for i in range(len(self.data_map[index])):
+            if self.data_map[index][i][0] == key:
+                return self.data_map[index][i][1];
+        return None;
+
+    def get_keys(self):
+        keys = [];
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    keys.append(self.data_map[i][j][0])
+        return keys;
+
+
 my_hash = Hash_Table(11);
 my_hash.set_item("Emanuel", 36);
 my_hash.set_item("Marika", 37);
 my_hash.set_item("Cats", 12);
 
+print(my_hash.get_keys())
 
-my_hash.print_table()
