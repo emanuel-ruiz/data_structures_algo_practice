26522974d4bce165bb2a04f6b6be420471d8ff38
Emanuel Ruiz <emanuel.u.ruiz@gmail.com>
2024-05-13 19:30:47 -0700

    create find_duplicates method

---
+++ hash_tables/find_duplicates.py
@@ -0,0 +1,38 @@
+def find_duplicates(list1):
+
+    dupe = {}
+    items = []
+    for item in list1:
+        if item in dupe:
+            dupe[item] = True;
+        else:
+            dupe[item] = False;
+
+    for key in dupe.keys():
+        if dupe[key] == True:
+            items.append(key);
+    return items;
+
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    []
+    [1, 2]
+    [1]
+    [3, 4]
+    [1, 2, 3]
+    [1, 2, 3]
+    []
+
+"""
