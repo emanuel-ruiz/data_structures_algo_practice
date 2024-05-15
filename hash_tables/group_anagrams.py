90e3aaea62d9bebd99b22699aaa3ea9d68677ca7
Emanuel Ruiz <emanuel.u.ruiz@gmail.com>
2024-05-15 09:24:33 -0700

    create method that returns list of anagram words

--- hash_tables/group_anagrams.py
+++ hash_tables/group_anagrams.py
@@ -2,19 +2,30 @@
  Write a function that groups the anagrams in the array together. The function should return a list of
  lists, where each inner list contains a group of anagrams"""
 
-def group_anagrams(wordList):
-    finalList = [];
-    for i in range(len(wordList)):
-        word = {}
-        for letter in wordList[i]:
-            
+
+def group_anagrams(list1):
+    word_vals = {}; # will use the word values as the keys and a list with all words of 
+                    # the same value as the values
+    anagrams = [];
+
+    #Iterate for each word in the list and get it's numerical value
+    for word in list1:
+        word_val = 0;
+        for letter in word:
+            word_val += ord(letter)
+        #add word into the list of that value
+        if word_val in word_vals:
+            word_vals[word_val].append(word);
+        #otherwise create a new list with the word
+        else:
+            word_vals[word_val] = [word];
+
     
+    for key in word_vals.keys():
+        anagrams.append(word_vals[key]);
 
-def print_dictionaries(dictlist):
-    for key in dictlist.keys():
-        print(key + ":");
-        for lKey in dictlist[key].keys():
-            print(lKey);
+    return anagrams;
 
 
-group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+
+print(group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
