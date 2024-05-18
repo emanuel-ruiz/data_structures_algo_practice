008155350946d525694b092f45ab76c400b24bc4
Emanuel Ruiz <emanuel.u.ruiz@gmail.com>
2024-05-18 10:10:37 -0700

    add comments to longest consec sequence and code improvement

--- hash_tables/longest_consec_seq.py
+++ hash_tables/longest_consec_seq.py
@@ -2,16 +2,17 @@
 write a function that finds the length of the  longest_consecutive_sequence 
 (i.e., sequence of integers in which each element is one greater than the previous element). """
 
+#method converts list into a set 
+#this allows the ability to check in O(1) time if the following value is present in the list
 def longest_consecutive_sequence(list1):
     set_list = set(list1);
-    longest = 0;
+    longest = 0; # longest value
     for num in list1:
-        length = 0;
-        temp = num;
-        while temp in set_list:
+        length = 0; #length of current sequence
+        while num in set_list: #loop until the sequence is broken
             length += 1
-            temp = temp + 1;
-        if length > longest:
+            num = num + 1;
+        if length > longest: #set longest if current lenght sequence is greater. 
             longest = length;
     return longest;
 
